55988c17b772c5074c003312f37fcf1a237ee896
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2023-12-21 11:24:20 +0000

    Minor addition in template

--- src/template/pt2.test.ts
+++ src/template/pt2.test.ts
@@ -1,3 +1,8 @@
+import { part2 } from "./pt2";
+
 describe("Day x - Part 2", () => {
-  it("should answer correctly", () => {});
+  it("should answer correctly", async () => {
+    const actual = await part2();
+    expect(actual).toEqual("------");
+  });
 });
