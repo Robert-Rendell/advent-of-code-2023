ddebd34f77d31e853cfd67c1324ffbc705db4561
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2023-12-21 11:21:28 +0000

    Fix read file test

--- src/utils/read-file.test.ts
+++ src/utils/read-file.test.ts
@@ -2,6 +2,7 @@ import { readFile } from "./read-file";
 
 describe("Day 1 - Part 1", () => {
   it("should answer the example correctly", async () => {
-    await readFile("./test.txt");
+    const result = await readFile("./src/utils/test.txt");
+    expect(result.split("\n")).toEqual(["hello", "multiline", "foobar"])
   });
 });
