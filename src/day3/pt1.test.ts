8ad807b8b9d9d83f61aa6efbbeb5b28a6ec20be3
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2023-12-22 06:16:56 +0000

    Day 3 Part 1 completed

--- src/day3/pt1.test.ts
+++ src/day3/pt1.test.ts
@@ -2,12 +2,16 @@ import { part1 } from "./pt1";
 
 describe("Day x - Part 1", () => {
   it("should answer the puzzle correctly", async () => {
-    const actual = await part1();
+    const actual = await part1({
+      puzzleFilePath: "src/day3/puzzles/d3-puzzle.txt",
+    });
     expect(actual).toEqual("------");
   });
 
   it("should answer the example correctly", async () => {
-    const actual = await part1({ input: "src/day3/puzzles/d3-example.txt" });
+    const actual = await part1({
+      puzzleFilePath: "src/day3/puzzles/d3-example.txt",
+    });
     expect(actual).toEqual(4361);
   });
 });
--- src/day3/pt1.ts
+++ src/day3/pt1.ts
@@ -1,13 +1,28 @@
 import { readFile } from "../utils/read-file";
 
-type PartNumber = {
+class PartNumber {
   cells: EngineSchematicCell[];
-  value: number;
-};
-type EngineSchematicCell = {
+
+  constructor() {
+    this.cells = [];
+  }
+
+  get value() {
+    return this.cells.map((c) => c.value).join("");
+  }
+  public numberValue() {
+    return parseInt(this.value);
+  }
+}
+
+type Cell = {
   x: number;
   y: number;
 };
+type EngineSchematicCell = Cell & {
+  value: string;
+};
+type Coord = [number, number];
 class EngineSchematic extends Array<Array<string>> {
   constructor(input: string) {
     super(...EngineSchematic.parseEngineSchematic(input));
@@ -24,28 +39,105 @@ class EngineSchematic extends Array<Array<string>> {
     return engineSchematic;
   }
 
-  public neighbours(args: { x: number; y: number }): string[] {
-    return [];
+  private addNeighbourIfValid(neighbour: Coord, n: Coord[]) {
+    if (
+      typeof this[neighbour[0]] !== "undefined" &&
+      typeof this[neighbour[0]][neighbour[1]] !== "undefined"
+    ) {
+      n.push(neighbour);
+    }
+  }
+
+  public neighbours(cell: Cell): Coord[] {
+    const n: Coord[] = [];
+
+    const potentialNeighbours: Coord[] = [
+      [cell.x + 1, cell.y - 1],
+      [cell.x - 1, cell.y - 1],
+      [cell.x, cell.y - 1],
+      [cell.x + 1, cell.y + 1],
+      [cell.x - 1, cell.y + 1],
+      [cell.x, cell.y + 1],
+      [cell.x + 1, cell.y],
+      [cell.x - 1, cell.y],
+    ];
+
+    potentialNeighbours.forEach((each) => {
+      this.addNeighbourIfValid(each, n);
+    });
+
+    return n;
+  }
+
+  public neighbouringCells(cell: Cell) {
+    const ns = this.neighbours(cell);
+    return ns.map((coord) => {
+      const esc: EngineSchematicCell = {
+        x: coord[0],
+        y: coord[1],
+        value: this[coord[0]][coord[1]],
+      };
+      return esc;
+    });
+  }
+
+  private isSymbolOrDot(value: string) {
+    return isNaN(parseInt(value));
+  }
+
+  private isSymbol(value: string) {
+    return isNaN(parseInt(value)) && value !== ".";
+  }
+
+  private getPotentialPartNumbers(): PartNumber[] {
+    const potentialPartNumbers: PartNumber[] = [];
+    this.forEach((each, x) => {
+      let currentPartNumber = new PartNumber();
+      each.forEach((numberSymbolOrDot, y) => {
+        const isSymbolOrDot = this.isSymbolOrDot(numberSymbolOrDot);
+        if (isSymbolOrDot) {
+          if (currentPartNumber.value) {
+            potentialPartNumbers.push(currentPartNumber);
+            currentPartNumber = new PartNumber();
+          }
+        } else {
+          currentPartNumber.cells.push({
+            x,
+            y,
+            value: numberSymbolOrDot,
+          });
+        }
+      });
+      // Not a part number across two lines
+      if (currentPartNumber.value) {
+        potentialPartNumbers.push(currentPartNumber);
+      }
+      currentPartNumber = new PartNumber();
+    });
+    return potentialPartNumbers;
   }
 
   public getPartNumbers(): PartNumber[] {
-    const partNumbers = [];
-    this.forEach((each, i) => {
-      each.forEach((numberSymbolOrDot, j) => {});
+    const ppns = this.getPotentialPartNumbers();
+    return ppns.filter((ppn) => {
+      let isPartNumber = false;
+
+      isPartNumber = ppn.cells.some((cell) => {
+        const ns = this.neighbouringCells(cell);
+        return ns.some((n) => this.isSymbol(n.value));
+      });
+
+      return isPartNumber;
     });
-    return [
-      {
-        value: 0,
-        cells: [],
-      },
-    ];
   }
 }
 
-export async function part1(opts?: { input: string }) {
+export async function part1(
+  opts: { input: string } | { puzzleFilePath: string },
+) {
   let input: string = "";
-  if (!opts?.input) {
-    input = await readFile("src/day3/puzzles/d3-puzzle.txt");
+  if ("puzzleFilePath" in opts) {
+    input = await readFile(opts.puzzleFilePath);
   } else {
     input = opts.input;
   }
@@ -54,5 +146,5 @@ export async function part1(opts?: { input: string }) {
 
   const partNumbers = engineSchematic.getPartNumbers();
 
-  return partNumbers.map((pn) => pn.value).reduce((p, c) => p + c);
+  return partNumbers.map((pn) => pn.numberValue()).reduce((p, c) => p + c);
 }
