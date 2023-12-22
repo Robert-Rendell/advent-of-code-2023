e5f0b6f11d328320272b8ce373db6c2b2966ee90
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2023-12-22 20:18:09 +0000

    Add day 4 puzzles

--- src/day4/pt1.test.ts
+++ src/day4/pt1.test.ts
@@ -1,16 +1,16 @@
 import { part1 } from "./pt1";
 
-describe("Day x - Part 1", () => {
+describe("Day 4 - Part 1", () => {
   it("should answer the puzzle correctly", async () => {
     const actual = await part1({
-      puzzleFilePath: "src/dayX/puzzles/dX-puzzle.txt",
+      puzzleFilePath: "src/day4/puzzles/d4-puzzle.txt",
     });
     expect(actual).toEqual("------");
   });
 
   it("should answer the example correctly", async () => {
     const actual = await part1({
-      puzzleFilePath: "src/dayX/puzzles/dX-example.txt",
+      puzzleFilePath: "src/day4/puzzles/d4-example.txt",
     });
     expect(actual).toEqual("------");
   });
