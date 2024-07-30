c91b9b7fa1fd9a62b8303993418b01a2fc270574
Rob R <rob_452@hotmail.co.uk>
2024-07-30 22:06:45 +0100

    Adjust solution template to have the example test first; in order of completion.

--- src/template/pt1.test.ts
+++ src/template/pt1.test.ts
@@ -1,16 +1,16 @@
 import { part1 } from "./pt1";
 
 describe("Day x - Part 1", () => {
-  it("should answer the puzzle correctly", async () => {
+  it("should answer the example correctly", async () => {
     const actual = await part1({
-      puzzleFilePath: "src/dayX/puzzles/dX-puzzle.txt",
+      puzzleFilePath: "src/dayX/puzzles/dX-example.txt",
     });
     expect(actual).toEqual("------");
   });
 
-  it("should answer the example correctly", async () => {
+  it("should answer the puzzle correctly", async () => {
     const actual = await part1({
-      puzzleFilePath: "src/dayX/puzzles/dX-example.txt",
+      puzzleFilePath: "src/dayX/puzzles/dX-puzzle.txt",
     });
     expect(actual).toEqual("------");
   });
--- src/template/pt2.test.ts
+++ src/template/pt2.test.ts
@@ -1,16 +1,16 @@
 import { part2 } from "./pt2";
 
 describe("Day x - Part 2", () => {
-  it("should answer the puzzle correctly", async () => {
+  it("should answer the example correctly", async () => {
     const actual = await part2({
-      puzzleFilePath: "src/dayX/puzzles/dX-puzzle.txt",
+      puzzleFilePath: "src/dayX/puzzles/dX-example.txt",
     });
     expect(actual).toEqual("------");
   });
 
-  it("should answer the example correctly", async () => {
+  it("should answer the puzzle correctly", async () => {
     const actual = await part2({
-      puzzleFilePath: "src/dayX/puzzles/dX-example.txt",
+      puzzleFilePath: "src/dayX/puzzles/dX-puzzle.txt",
     });
     expect(actual).toEqual("------");
   });
