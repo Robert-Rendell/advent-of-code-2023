9d0d5bfd67ae7639e2e4635962966e9110b64682
Rob R <rob_452@hotmail.co.uk>
2024-07-30 22:09:22 +0100

    Folder name change and adjust read file util unit test description

--- src/utils/read-file.test.ts
+++ src/utils/read-file.test.ts
@@ -1,7 +1,7 @@
 import { readFile } from "./read-file";
 
-describe("Day 1 - Part 1", () => {
-  it("should answer the example correctly", async () => {
+describe("Read file", () => {
+  it("should read test.txt correctly", async () => {
     const result = await readFile("./src/utils/test.txt");
     expect(result.split("\n")).toEqual(["hello", "multiline", "foobar"]);
   });
