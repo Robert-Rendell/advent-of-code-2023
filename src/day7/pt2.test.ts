477f0aed7afddce6993254365ae8e4fcc5dba8e4
Rob R <rob_452@hotmail.co.uk>
2024-08-01 16:39:45 +0100

    Day 7 Pt 2 - do more unit testing with available data

--- src/day7/pt2.test.ts
+++ src/day7/pt2.test.ts
@@ -7,20 +7,36 @@ describe("Day 7 - Part 2", () => {
     beforeEach(() => {
       CamelCardGame.switchToJokerGameMode();
     });
+    it("should calculate 32T3K -> 32T3K (the same)", async () => {
+      const hand = new CamelCardsHand({ hand: "32T3K", bid: 0 });
+      expect(hand.jokerHand()).toEqual("32T3K");
+      expect(hand.handType()).toEqual("One pair");
+    });
+    it("should calculate KK677 -> KK677 (the same)", async () => {
+      const hand = new CamelCardsHand({ hand: "KK677", bid: 0 });
+      expect(hand.jokerHand()).toEqual("KK677");
+      expect(hand.handType()).toEqual("Two pair");
+    });
     it("should calculate T55J5 -> T5555", async () => {
       const hand = new CamelCardsHand({ hand: "T55J5", bid: 0 });
       expect(hand.jokerHand()).toEqual("T5555");
+      expect(hand.jokerHandType()).toEqual("Four of a kind");
+      expect(hand.handType()).toEqual("Three of a kind");
     });
 
     it("should calculate KTJJT -> KTTTT", async () => {
       const hand = new CamelCardsHand({ hand: "KTJJT", bid: 0 });
       // T is stronger than J in the joker game
       expect(hand.jokerHand()).toEqual("KTTTT");
+      expect(hand.jokerHandType()).toEqual("Four of a kind");
+      expect(hand.handType()).toEqual("Two pair");
     });
 
     it("should calculate QQQJA -> QQQQA", async () => {
       const hand = new CamelCardsHand({ hand: "QQQJA", bid: 0 });
       expect(hand.jokerHand()).toEqual("QQQQA");
+      expect(hand.jokerHandType()).toEqual("Four of a kind");
+      expect(hand.handType()).toEqual("Three of a kind");
     });
   });
 
