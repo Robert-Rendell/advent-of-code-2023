b560ad24f6abb1868e842fdbac57329feb23ae99
Rob R <rob_452@hotmail.co.uk>
2024-08-01 16:33:42 +0100

    Tidy up

--- src/day7/pt2.test.ts
+++ src/day7/pt2.test.ts
@@ -4,22 +4,22 @@ import { part2 } from "./pt2";
 
 describe("Day 7 - Part 2", () => {
   describe("joker hands", () => {
+    beforeEach(() => {
+      CamelCardGame.switchToJokerGameMode();
+    });
     it("should calculate T55J5 -> T5555", async () => {
       const hand = new CamelCardsHand({ hand: "T55J5", bid: 0 });
-      CamelCardGame.switchToJokerGameMode();
       expect(hand.jokerHand()).toEqual("T5555");
     });
 
     it("should calculate KTJJT -> KTTTT", async () => {
       const hand = new CamelCardsHand({ hand: "KTJJT", bid: 0 });
       // T is stronger than J in the joker game
-      CamelCardGame.switchToJokerGameMode();
       expect(hand.jokerHand()).toEqual("KTTTT");
     });
 
     it("should calculate QQQJA -> QQQQA", async () => {
       const hand = new CamelCardsHand({ hand: "QQQJA", bid: 0 });
-      CamelCardGame.switchToJokerGameMode();
       expect(hand.jokerHand()).toEqual("QQQQA");
     });
   });
